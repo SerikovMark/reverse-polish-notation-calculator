23d2ac622d56d9e076d10890d63820183455d2c3
Mark Sierikov <serikov.lg@gmail.com>
2018-12-05 11:00:44 +0200

    Added run method.

---
+++ src/main/java/com/calculator/rpn/Main.java
@@ -0,0 +1,16 @@
+package com.calculator.rpn;
+
+public class Main {
+    public static void main(String[] args) {
+        InitEnv();
+        StartApp();
+    }
+
+    private static void StartApp() {
+
+    }
+
+    private static void InitEnv() {
+
+    }
+}
